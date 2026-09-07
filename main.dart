02227ef511da83448d40368e6cb1b49ea8890f3c
tokaahmed354 <tokaahmed34567@gmail.com>
2026-09-07 20:05:00 +0200

    creational pattern done

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 import 'creational/creational/singleton/singleton.dart';
-import 'creational/factory_method/email_notification_creator.dart';
-import 'creational/factory_method/notification_creator.dart';
-import 'creational/factory_method/sms_notification_creator.dart';
+import 'creational/creational/factory_method/email_notification_creator.dart';
+import 'creational/creational/factory_method/notification_creator.dart';
+import 'creational/creational/factory_method/sms_notification_creator.dart';
 
 void main() {
   final config1 = AppConfig();
